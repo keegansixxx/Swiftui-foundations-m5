40b77337f17d826908b64619a26d9c92b29e90eb
Keegan Pangowish <keegan>
2021-12-26 21:46:27 -0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Keegan Pangowish on 2021-12-26.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
